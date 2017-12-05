dd3c835ad830da257e10d4a528050ddae88cf96f
Rasa <fiammara@gmail.com>
2017-12-05 09:14:34 +0200

    daugyba ir dalyba ideta

--- skaiciuotuvas/src/skaiciuotuvas1/Main.java
+++ skaiciuotuvas/src/skaiciuotuvas1/Main.java
@@ -46,13 +46,26 @@ reader.close();
         return root;
 	}
 
-	private static void mult() {
+	private static int mult() {
 		// TODO Auto-generated method stub
-		
+		Scanner in = new Scanner(System.in);
+        System.out.println("Enter your first integer value: ");
+        int first = in.nextInt();
+        System.out.println("Enter your second integer value: ");
+        int second = in.nextInt();
+        int mult = first * second;
+        return mult;
 	}
 
-	private static void div() {
+	private static double div() {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        System.out.println("Enter your first integer value: ");
+        int first = in.nextInt();
+        System.out.println("Enter your second integer value: ");
+        int second = in.nextInt();
+        int div = first/second;
+        return div;
 		
 	}
 
