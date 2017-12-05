e71e88a738762154e69c17771f310eed31296c51
Rasa <fiammara@gmail.com>
2017-12-05 08:55:50 +0200

    skaičiuotuvo meniu įdėtas

---
+++ skaiciuotuvas/src/skaiciuotuvas1/Main.java
@@ -0,0 +1,18 @@
+package skaiciuotuvas1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner reader =new Scanner(System.in);
+System.out.println("Sumuoti 1:");
+System.out.println("Atimti 2:");
+System.out.println("Dalinti 3:");
+System.out.println("Dauginti 4:");
+System.out.println("Traukti ðakná 5:");
+System.out.println("Kelt5i kvadratu 6:");
+	}
+
+}
